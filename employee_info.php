7f38a590ad83f433e665d2d7f9db17b9c9af92cc
SaerElMasri <saer1890@gmail.com>
2023-03-11 20:07:00 +0200

    Added employee registration

--- employee_info.php
+++ employee_info.php
@@ -5,8 +5,9 @@
     $date_joined = $_POST['date_joined'];
     $position = $_POST['position'];
     $hospital_id = $_POST['hospital_id'];
-    $query = $conn ->prepare('insert into employees_info(user_id, SSN, date_joined, position, hospital_id) values (?,?,?, ?)');
+    $query = $conn ->prepare('insert into employees_info(user_id, SSN, date_joined, position, hospital_id) values (?,?,?,?,?)');
     $query -> bind_param('isssi', $user_id, $SSN, $date_joined, $position, $hospital_id);
     $query -> execute();
-    $response['Status'] = 'Success';
+    $response['status'] = 'Success';
+    echo json_encode($response);
 ?>
